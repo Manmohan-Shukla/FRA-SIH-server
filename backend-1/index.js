520c00cb11662938aeb9521178f01c58ee735e8b
Manmohan Shukla <162003822+Manmohan-Shukla@users.noreply.github.com>
2025-09-19 02:08:00 +0530

    Update index.js

--- backend-1/index.js
+++ backend-1/index.js
@@ -3,16 +3,17 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const userRouter = require("../backend-1/routes/index");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
 app.use("/user", userRouter);
 
-mongoose.connect('mongodb+srv://manmohanshukla815:Man%401234@cluster0.fjnpqjp.mongodb.net/FRA-server', { dbName: "FRA-server" })
+mongoose.connect(process.env.MONGO_URL, { dbName: "FRA-server" })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(PORT, () => console.log('Server running on port 3000'));
